1d0ac659f37e37d8688b63761c41d4d1d9c5d341
BorjaLia <borja.lia.deichler@gmail.com>
2026-08-18 23:11:38 -0300

    Call Post Build Event

--- config/config.bat
+++ config/config.bat
@@ -14,7 +14,7 @@ if not exist %out%\ (
 	echo copying library dependencies at
 
 	echo %path%lib\glfw-3.5.1.bin.WIN64\lib-static-ucrt
-	copy "%path%lib\glfw-3.5.1.bin.WIN64\lib-static-ucrt*.dll" "%out%" /y
+	copy "%path%lib\glfw-3.5.1.bin.WIN64\lib-static-ucrt\*.dll" "%out%" /y
 )
 
 echo -Inital configuration done-
