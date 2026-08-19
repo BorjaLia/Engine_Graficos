f0dbe20245e948d18277b773158f8a519c9e1def
BorjaLia <borja.lia.deichler@gmail.com>
2026-08-18 23:09:32 -0300

    Add config.bat

---
+++ config/config.bat
@@ -0,0 +1,20 @@
+echo -Running inital configuration-
+
+set path=%~1
+set out=%~2
+
+echo using %path%
+echo and %out%
+
+cd %path%
+
+echo step 1
+
+if not exist %out%\ (
+	echo copying library dependencies at
+
+	echo %path%lib\glfw-3.5.1.bin.WIN64\lib-static-ucrt
+	copy "%path%lib\glfw-3.5.1.bin.WIN64\lib-static-ucrt*.dll" "%out%" /y
+)
+
+echo -Inital configuration done-
